eaf9670eeb650b72099046b86eff9d565f7bfc41
TheRealKevBot <Kglas001@gmail.com>
2021-06-12 10:55:39 -0600

    First

---
+++ index.js
@@ -0,0 +1,15 @@
+// Write a function that when given a URL as a string, 
+// parses out just the domain name and returns it as a string. 
+
+function domainName(url){
+
+}
+
+console.log(domainName("http://google.com"), "Answer: google");
+console.log(domainName("http://google.co.jp"), "Answer: google");
+console.log(domainName("www.xakep.ru"), "Answer: xakep");
+console.log(domainName("https://youtube.com"), "Answer: youtube");
+
+
+
+
